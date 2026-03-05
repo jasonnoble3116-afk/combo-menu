51993bdbe49126e783380c1c0a7d2c47380c0270
Jason Noble <jasonnoble3116@gmail.com>
2026-03-05 17:21:42 +0000

    type of taco/shell

---
+++ main.py
@@ -0,0 +1,16 @@
+# Iteration 1
+choice=input("what kind of taco would you like? chicken $5.25, fish $5.75, steak $6.25")
+print("what kind of taco would you like? chicken $5.25, fish $5.75, steak $6.25")
+if choice=="steak":
+    print("Steak $6.25")
+elif choice=="chicken":
+    print("chicken $5.25")
+elif choice=="fish":
+    print("fish $5.75")
+
+choice=input("what type of shell would you like? hard or soft?")
+print("what type of shell would you like? hard or soft?")
+if choice=="hard":
+    print("hard shell")
+elif choice=="soft":
+    print("soft")
