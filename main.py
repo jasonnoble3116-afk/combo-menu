56f1b67edb332a31acd890f99ffae6b167b37d24
Jason Noble <jasonnoble3116@gmail.com>
2026-03-09 16:30:54 +0000

    adds third and fourth iterations of plan

--- main.py
+++ main.py
@@ -15,6 +15,24 @@ if choice=="hard":
     print("hard shell")
 elif choice=="soft":
     print("soft shell")
+choice = input("how many tacos would you like? one, two $0.50, three $1.00, four $1.50, five $2.00, six $2.50").strip()
+if choice == "one":
+    print("one taco")
+elif choice == "two":
+    print("two tacos $0.50")
+    Total_cost += 0.50
+elif choice == "three":
+    print("three tacos $1.00")
+    Total_cost += 1.00
+elif choice == "four":
+    print("four tacos $1.50")
+    Total_cost += 1.50
+elif choice == "five":
+    print("five tacos $2.00")
+    Total_cost += 2.00
+elif choice == "six":
+    print("six tacos $2.50")
+    Total_cost += 2.50
 
 # Iteration 2
 choice = input("would you like a beverage?").strip().lower()
@@ -29,6 +47,15 @@ if choice == "yes":
     elif choice == "large":
         print("large $2.25")
         Total_cost += 2.25
+if choice == "no":
+    print("no beverage")
+choice = input("what type of beverage would you like? sode, sweet tea, or water?")
+if choice == "soda":
+    print("soda")
+elif choice == "sweet tea":
+    print("sweet tea")
+elif choice == "water":
+    print("water")
 
 # Iteration 3
 choice = input("would you like a bag of chips?").strip().lower()
@@ -43,6 +70,22 @@ if choice == "yes":
     elif choice == "large":
         print("large $2.00")
         Total_cost += 2.00
+
+#Iteration 4
+choice = input("would you like any sauces for you tacos?").strip().lower()
+if choice == "yes":
+    choice = input("what sauce would you like? buffalo sauce packets, salsa $0.25, guacamole $0.25, melted cheese $0.50").strip().lower()
+    if choice == "buffalo sauce":
+        print("buffalo sauce")
+    elif choice == "salsa":
+        print("salsa")
+        Total_cost += 0.25
+    elif choice == "guacamole":
+        print("guacamole")
+        Total_cost += 0.25
+    elif choice == "melted cheese":
+        print("melted cheese")
+        Total_cost += 0.50
 # final checkout prompts
 choice = input("are you ready for checkout?").strip().lower()
 if choice == "yes":
@@ -55,7 +98,8 @@ elif choice == "no":
         if choice == "yes":
             print("large beverage $2.25 and large bag of chips $2.00")
             Total_cost += 2.25 + 2.00
+            print(f"Final total: ${Total_cost:.2f}")
         if choice == "no":
-            print("proceed to checkout")
+            print(f"Final total: ${Total_cost:.2f}")
     else:
         print(f"Final total: ${Total_cost:.2f}")
